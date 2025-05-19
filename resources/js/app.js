4f85db325e93d0acb3bf01e87f59fc7e2f35001f
Arzel John Zolina <Arzeljrz17@gmail.com>
2025-05-20 00:50:17 +0800

    add light mode to darkmode

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,39 @@
-import './bootstrap';
-
-import Alpine from 'alpinejs';
+import "./bootstrap";
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+// DARK MODE TOGGLE BUTTON
+const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+// Force dark mode as default if no theme is set
+if (!localStorage.getItem("color-theme")) {
+    localStorage.setItem("color-theme", "dark");
+    document.documentElement.classList.add("dark");
+}
+
+// Show correct icon based on theme
+if (localStorage.getItem("color-theme") === "dark") {
+    document.documentElement.classList.add("dark");
+    themeToggleLightIcon?.classList.remove("hidden");
+} else {
+    document.documentElement.classList.remove("dark");
+    themeToggleDarkIcon?.classList.remove("hidden");
+}
+
+const themeToggleBtn = document.getElementById("theme-toggle");
+
+themeToggleBtn?.addEventListener("click", function () {
+    themeToggleDarkIcon?.classList.toggle("hidden");
+    themeToggleLightIcon?.classList.toggle("hidden");
+
+    if (localStorage.getItem("color-theme") === "light") {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+    } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+    }
+});
